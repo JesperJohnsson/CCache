3a3fae21d366b849dcfc67171a0e23d25ff4f400
JesperJohnsson <jesper.johnsson1995@hotmail.com>
2015-05-09 02:43:50 +0200

    v 1.0

--- src/ccache/CCache.php
+++ src/ccache/CCache.php
@@ -1,74 +1,105 @@
 <?php
-namespace phpe\cache;
+
+namespace jejd14\ccache;
+
+
+
 /**
 * A class for caching objects.
 *
 */
-class CCache {
+class CCache
+{
+
+
+
     /**
     * Properties
     */
     public $options;
+
+
+
     /**
     * Constructor.
     *
     * @param $text string the dirty HTML.
+    *
     * @return string as the clean HTML.
     */
-    public function __construct($dir) {
-        $this->options = array(
+    public function __construct($dir)
+    {
+        $this->options = [
             'age' => 7 * 24 * 60 * 60,
             'dir' => $dir,
-        );
-        if(!is_dir($dir)){
+        ];
+        if (!is_dir($dir)) {
             $res = mkdir($dir, 0777, true);
-            if(!$res) {
+            if (!$res) {
                 throw new Exception("Failed to create {$dir}");
             }
         }
     }
+
+
+
     /**
     * Get an item from the cache it its there.
     *
     * @param string $key to the cached object.
+    *
     * @return mixed the cached object or false if it has aged or null if it does not exists.
     */
-    public function Get($key) {
+    public function get($key)
+    {
         $file = $this->options['dir'] . '/' . strtolower($key);
-        if(is_file($file)) {
-            if(filemtime($file) + $this->options['age'] > time()) {
+        if (is_file($file)) {
+            if (filemtime($file) + $this->options['age'] > time()) {
                 return unserialize(file_get_contents($file));
             }
             return false;
         }
         return null;
     }
+
+
+
     /**
     * Put an item to the cache.
     *
     * @param string $key to the cached object.
+    *
     * @throws Exception if failing to write to cache.
     */
-    public function Put($key, $item) {
+    public function put($key, $item)
+    {
         $file = $this->options['dir'] . '/' . strtolower($key);
-        if(!file_put_contents($file, serialize($item))) {
+        if (!file_put_contents($file, serialize($item))) {
             throw new Exception("Failed writing cache object '{$key}'.");
         }
     }
+
+
+
     /**
     * Prune a item from cache.
     *
     * @param string $key to the cached object.
     */
-    public function Prune($key) {
+    public function prune($key)
+    {
         @unlink($this->options['dir'] . '/' . strtolower($key));
     }
+
+
+
     /**
     * Prune all items from cache.
     *
     * @return int number of items removed.
     */
-    public function PruneAll() {
+    public function pruneAll()
+    {
         $files = glob($this->options['dir'].'/*');
         $items = count($files);
         array_map('unlink', $files);
