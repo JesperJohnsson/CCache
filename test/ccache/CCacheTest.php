e040d7d8c2ae6d58ccc7b824c8bbe262f49705b6
JesperJohnsson <jesper.johnsson1995@hotmail.com>
2015-05-10 17:21:42 +0200

    v 1.0

--- test/ccache/CCacheTest.php
+++ test/ccache/CCacheTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace jejd14\ccache;
 /**
 * A class for caching objects.
@@ -13,15 +14,30 @@ class CCacheTest extends \PHPUnit_Framework_TestCase {
         throw new InvalidArgumentException('Failed to create hejsan', 'hejsan');
     }
     */
-    public function testCCache()
+    public function testGetCCache()
     {
         $el = new \jejd14\ccache\CCache('hejsan');
         $fil = $el->get('fil');
         $this->assertNull($fil);
     }
+
+    public function testPutCCache()
+    {
+        $el = new \jejd14\ccache\CCache('hejsan');
+        $fil = $el->put('fil', 'item');
+        $this->assertNull($fil);
+    }
+
     public function testPruneCCache()
     {
-        $el = new \phpe\cache\CCache('hejsan');
+        $el = new \jejd14\ccache\CCache('hejsan');
+        $fil = $el->prune('fil');
+        $this->assertEquals($fil, 0);
+    }
+
+    public function testPruneAllCCache()
+    {
+        $el = new \jejd14\ccache\CCache('hejsan');
         $fil = $el->pruneAll();
         $this->assertEquals($fil, 0);
     }
